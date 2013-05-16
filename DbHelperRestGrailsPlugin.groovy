436972a22cb7d7b762b37b9b7beca3015ca98e38
Burt Beckwith <burt@burtbeckwith.com>
2013-05-16 16:43:52 -0400

    cleanup

--- DbHelperRestGrailsPlugin.groovy
+++ DbHelperRestGrailsPlugin.groovy
@@ -2,17 +2,10 @@ class DbHelperRestGrailsPlugin {
 
     def version = "0.1"
     def grailsVersion = "2.0 > *"
-    def dependsOn = [:]
-    def pluginExcludes = [
-            "grails-app/views/error.gsp"
-    ]
-
     def title = "DB Helper - Rest"
     def author = "Ondrej Kvasnovsky"
     def authorEmail = "ondrej.kvasnovsky@gmail.com"
-    def description = '''\
-Provides methods for dropping, creating and initialization of database schema from the project bootstrap.
-'''
+    def description = 'Provides methods for dropping, creating and initialization of database schema from the project bootstrap.'
 
     def documentation = "http://grails.org/plugin/grails-plugin-db-helper-rest"
 
@@ -21,25 +14,4 @@ Provides methods for dropping, creating and initialization of database schema fr
     def issueManagement = [system: "GIT", url: "https://github.com/ondrej-kvasnovsky/grails-plugin-db-helper-rest/issues"]
 
     def scm = [url: "http://grails.org/plugin/grails-plugin-db-helper-rest"]
-
-    def doWithWebDescriptor = { xml ->
-    }
-
-    def doWithSpring = {
-    }
-
-    def doWithDynamicMethods = { ctx ->
-    }
-
-    def doWithApplicationContext = { applicationContext ->
-    }
-
-    def onChange = { event ->
-    }
-
-    def onConfigChange = { event ->
-    }
-
-    def onShutdown = { event ->
-    }
 }
