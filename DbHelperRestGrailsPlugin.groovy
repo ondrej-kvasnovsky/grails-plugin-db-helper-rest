d3f51abead350d89900dd27de4986d8918257897
Ondrej Kvasnovsky <ondrej.kvasnovsky@gmail.com>
2013-05-17 09:26:40 +0200

    fixed URLs in the plugin details

--- DbHelperRestGrailsPlugin.groovy
+++ DbHelperRestGrailsPlugin.groovy
@@ -1,17 +1,17 @@
 class DbHelperRestGrailsPlugin {
 
-    def version = "0.1"
+    def version = "0.2"
     def grailsVersion = "2.0 > *"
     def title = "DB Helper - Rest"
     def author = "Ondrej Kvasnovsky"
     def authorEmail = "ondrej.kvasnovsky@gmail.com"
     def description = 'Provides methods for dropping, creating and initialization of database schema from the project bootstrap.'
 
-    def documentation = "http://grails.org/plugin/grails-plugin-db-helper-rest"
+    def documentation = "http://grails.org/plugin/db-helper-rest"
 
     def license = "APACHE"
 
     def issueManagement = [system: "GIT", url: "https://github.com/ondrej-kvasnovsky/grails-plugin-db-helper-rest/issues"]
 
-    def scm = [url: "http://grails.org/plugin/grails-plugin-db-helper-rest"]
+    def scm = [url: "https://github.com/ondrej-kvasnovsky/grails-plugin-db-helper-rest"]
 }
