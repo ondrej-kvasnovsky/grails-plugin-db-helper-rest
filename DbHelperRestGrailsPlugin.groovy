c734818d32cabe834587bfeb5a26b178fdefa153
Ondrej Kvasnovsky <ondrej.kvasnovsky@gmail.com>
2013-06-12 22:01:17 +0200

    0.5 released

--- DbHelperRestGrailsPlugin.groovy
+++ DbHelperRestGrailsPlugin.groovy
@@ -1,6 +1,6 @@
 class DbHelperRestGrailsPlugin {
 
-    def version = "0.4"
+    def version = "0.5"
     def grailsVersion = "2.0 > *"
     def title = "DB Helper - REST"
     def author = "Ondrej Kvasnovsky"
